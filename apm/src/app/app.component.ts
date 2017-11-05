cdfb0ac20dba952c568c2b41e994027af63661d9
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2017-11-05 14:00:57 -0500

    Create ProductList Component and associated files.

--- apm/src/app/app.component.ts
+++ apm/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div> 
-    <h1> {{pageTitle}}</h1>
-    <div> My first component </div>
-  </div>`
-
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent {
